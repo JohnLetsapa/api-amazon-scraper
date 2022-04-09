c4401769c743df23bcb152bc2600ee58d36b8dc5
Letsapa Letsapa <letsapaletsapa@Letsapas-MacBook-Pro.local>
2022-04-09 12:31:53 +0200

    made api key dynamic

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const request = require('request-promise');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const apiKey = '39ae850e42497cd43bee6e9366ee27d3';
-const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+// insert your scraper API key as an argument in the below function...
+const generateScraperUrl = (apiKey) =>
+  `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json()); // allows app to parse json input -> i.e. middleware - converts client data into json before it reaches the server
 
@@ -16,10 +18,11 @@ app.get('/', (req, res) => {
 // GET product details. Uses request method to fetch product details by id from params.
 app.get('/products/:id', async (req, res) => {
   const productId = req.params.id;
+  const { api_key } = req.query;
 
   try {
     const response = await request(
-      `${baseUrl}&url=https://www.amazon.com/dp/${productId}`
+      `${generateBaseUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`
     );
 
     res.status(200).json(JSON.parse(response));
@@ -31,10 +34,13 @@ app.get('/products/:id', async (req, res) => {
 // GET product reviews. Uses request method to fetch product reviews by id from params.
 app.get('/products/:id/reviews', async (req, res) => {
   const productId = req.params.id;
+  const { api_key } = req.query;
 
   try {
     const response = await request(
-      `${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`
+      `${generateBaseUrl(
+        api_key
+      )}&url=https://www.amazon.com/product-reviews/${productId}`
     );
 
     res.status(200).json(JSON.parse(response));
@@ -46,10 +52,13 @@ app.get('/products/:id/reviews', async (req, res) => {
 // GET product offers. Uses request method to fetch product offers by id from params.
 app.get('/products/:id/offers', async (req, res) => {
   const productId = req.params.id;
+  const { api_key } = req.query;
 
   try {
     const response = await request(
-      `${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+      `${generateBaseUrl(
+        api_key
+      )}&url=https://www.amazon.com/gp/offer-listing/${productId}`
     );
 
     res.status(200).json(JSON.parse(response));
@@ -61,10 +70,13 @@ app.get('/products/:id/offers', async (req, res) => {
 // GET search results. Uses request method to search by id from params.
 app.get('/search/:searchQuery', async (req, res) => {
   const { searchQuery } = req.params;
+  const { api_key } = req.query;
 
   try {
     const response = await request(
-      `${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`
+      `${generateBaseUrl(
+        api_key
+      )}&url=https://www.amazon.com/s?k=${searchQuery}`
     );
 
     res.status(200).json(JSON.parse(response));
